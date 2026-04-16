f84f035918e70c542c3839d30261df75ec098fbe
NiazXD <niaz.academic200@gmail.com>
2026-04-16 12:33:40 +0600

    Ami commit kori

---
+++ Assets/Script/PlayerLevel.cs
@@ -0,0 +1,16 @@
+using UnityEngine;
+
+public class PlayerLevel : MonoBehaviour
+{
+    private PlayerArmory armory;
+    void Start()
+    {
+        armory = GetComponent<PlayerArmory>();
+    }
+
+    
+    void Update()
+    {
+        
+    }
+}
