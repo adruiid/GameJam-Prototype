aacb0bbd1ff8455431ebc3616aba076a3576b9c3
adruiid <itsadreedffs@gmail.com>
2026-04-16 18:36:47 +0600

    Enemy Behavior updated

--- Assets/Script/Enemy Behavior.cs
+++ Assets/Script/Enemy Behavior.cs
@@ -2,18 +2,53 @@ using UnityEngine;
 
 public class EnemyBehavior : MonoBehaviour
 {   
+    [Header("Movement Settings")]
     [SerializeField] float moveSpeed = 5f;
+
+    [Header("Separation Settings")]
+    [SerializeField] float separationRadius = 1.5f;
+    [SerializeField] float separationWeight = 1.5f;
+    [SerializeField] LayerMask enemyLayer;
+
     GameObject player;
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         player = GameObject.Find("Player");
     }
 
-    // Update is called once per frame
     void Update()
     {
-        Vector3 direction = (player.transform.position - transform.position).normalized;
-        transform.Translate(direction * moveSpeed * Time.deltaTime, Space.World);
+        if (player == null) return;
+        Vector3 targetDirection = (player.transform.position - transform.position).normalized;
+        Vector3 separationDirection = GetSeparationVector();
+        Vector3 finalDirection = (targetDirection + separationDirection * separationWeight).normalized;
+        transform.Translate(finalDirection * moveSpeed * Time.deltaTime, Space.World);
+    }
+
+    Vector3 GetSeparationVector()
+    {
+        Vector3 separationForce = Vector3.zero;
+        int neighborsCount = 0;
+        Collider[] neighbors = Physics.OverlapSphere(transform.position, separationRadius, enemyLayer);
+
+        foreach (Collider neighbor in neighbors)
+        {
+            if (neighbor.gameObject == gameObject) continue;
+            Vector3 pushAway = transform.position - neighbor.transform.position;
+            float distance = pushAway.magnitude;
+            if (distance > 0)
+            {
+                separationForce += (pushAway.normalized / distance);
+                neighborsCount++;
+            }
+        }
+        if (neighborsCount > 0)
+        {
+            separationForce /= neighborsCount;
+        }
+        separationForce.y = 0;
+        
+        return separationForce;
     }
 }
